17cf59a959f75cf54d7f9868f505c4c4d8bd0c51
Line-B-333 <10187764@ucn.dk>
2025-10-27 11:34:17 +0100

    Added: Author

---
+++ MinieProjektS3/BlogLibrary/Model/Author.cs
@@ -0,0 +1,21 @@
+﻿namespace BlogDAO.Library.Model;
+
+public class Author
+{
+    #region Properties
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public string BlogTitle { get; set; }
+
+    #endregion
+
+    #region Construtor
+    public Author(string email, string passwordHash, string blogTitle)
+    {
+        Email = email;
+        PasswordHash = passwordHash;
+        BlogTitle = blogTitle;
+    } 
+    #endregion
+}
