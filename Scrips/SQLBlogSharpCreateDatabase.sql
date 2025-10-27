2a586c10054cddaa1033f94db5191f75f7b59163
Line-B-333 <10187764@ucn.dk>
2025-10-27 09:48:27 +0100

    Created database

---
+++ Scrips/SQLBlogSharpCreateDatabase.sql
@@ -0,0 +1,35 @@
+USE [BlogSharp]
+GO
+/****** Object:  Table [dbo].[Author]    Script Date: 27-10-2025 09:46:50 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Author](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Email] [nvarchar](100) NOT NULL,
+	[PasswordHash] [nvarchar](100) NOT NULL,
+	[BlogTitle] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Author] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[BlogPost]    Script Date: 27-10-2025 09:46:50 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[BlogPost](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[Title] [nvarchar](50) NOT NULL,
+	[TextBody] [nvarchar](max) NOT NULL,
+	[CreationDate] [datetime2](7) NOT NULL,
+	[FK_Author_Id] [int] NOT NULL,
+ CONSTRAINT [PK_BlogPost] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
